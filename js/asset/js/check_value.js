c2a23b144690d37da980893cc3919d003afba6bf
L-jy16 <95023009+spearboy@users.noreply.github.com>
2024-03-25 18:56:15 +0900

    update

--- js/asset/js/check_value.js
+++ js/asset/js/check_value.js
@@ -13,155 +13,9 @@ function commonCheckValue(btn) {
             if ($(item).val() == '') {
                 add_input_error_layer('Please enter', $(item))
                 test++;
-                // if ($(item).hasClass('emailInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('firstNameInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('lastNameInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('passwordInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('passwordConfirmInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('companyNameInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('phoneNumberInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('currentPasswordInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('newPasswordInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('confirmNewPasswordInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('contentInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('domainAddressInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('messengerIdInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('nameInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('contryInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('cityInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('zipCodeInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('streetAddressInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('paymentDateInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('cardNumberInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('cardHolderInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('expirationMoYeYInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('cvcInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('parnterCodeInput')) {
-                //     add_input_error_layer('Please enter', $(item))
-                //     test++;
-                // }
             } else if ($(item).val().length < $(item).attr('minlength')) {
                 add_input_error_layer('Too short', $(item))
                 test++;
-                // if ($(item).hasClass('emailInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('firstNameInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('lastNameInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('passwordInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('passwordConfirmInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('companyNameInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('phoneNumberInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('currentPasswordInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('newPasswordInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('confirmNewPasswordInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('contentInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('domainAddressInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('messengerIdInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('nameInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('contryInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('cityInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('zipCodeInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('streetAddressInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('paymentDateInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('cardNumberInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('cardHolderInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('expirationMoYeYInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('cvcInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // } else if ($(item).hasClass('parnterCodeInput')) {
-                //     add_input_error_layer('Too short', $(item))
-                //     test++;
-                // }
             } else if ($(item).hasClass('emailInput')) {
                 let regExp = /^[0-9a-zA-Z.;\-!#]+@[A-Za-z0-9\-]+\.[A-Za-z]+/;
                 if (!regExp.test($(item).val())) {
@@ -178,8 +32,6 @@ function commonCheckValue(btn) {
                     check++;
                 }
             }
-
-
         }
     })
     // btn.find('textarea').each(function (index, item) {
